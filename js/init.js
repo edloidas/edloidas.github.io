de249d2313d765a522f514706711f47a0a1c6ede
Mikita Taukachou <mta@enonic.com>
2015-07-20 17:47:39 +0300

    Add `debounce` function.

--- js/init.js
+++ js/init.js
@@ -13,32 +13,32 @@ function App() {
   function init() {
     this.background = document.getElementById( 'background' );
 
-  	this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+    this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
     this.renderer.setPixelRatio( window.devicePixelRatio );
     this.renderer.setSize( window.innerWidth, window.innerHeight );
     this.renderer.setClearColor( 0xffffff );
     background.appendChild( this.renderer.domElement );
 
-  	//
+    //
 
     this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
     this.camera.position.z = 400;
 
     this.scene = new THREE.Scene();
 
-  	var geometry = new THREE.IcosahedronGeometry( 100, 0 );
-  	var material = new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } );
-  	this.mesh = new THREE.Mesh( geometry, material );
+    var geometry = new THREE.IcosahedronGeometry( 100, 0 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } );
+    this.mesh = new THREE.Mesh( geometry, material );
 
     this.scene.add( this.mesh );
 
-  	//
+    //
 
-  	window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener( 'resize', debounce( onWindowResize, 64 ), false );
   }
 
   function onWindowResize() {
-  	this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize( window.innerWidth, window.innerHeight );
@@ -61,3 +61,29 @@ function App() {
 
 var edloidas = new App();
 edloidas.run();
+
+
+// UTILITIES
+
+/*
+ * debounce
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing.
+ * http://davidwalsh.name/javascript-debounce-function
+ */
+function debounce( func, wait, immediate ) {
+  var timeout;
+  return function() {
+    var that = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if ( !immediate ) func.apply( that, args );
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout( timeout );
+    timeout = setTimeout( later, wait );
+    if ( callNow ) func.apply( context, args );
+  };
+}
