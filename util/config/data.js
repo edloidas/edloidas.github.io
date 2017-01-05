caae500b5bc22f994c9e57c592f7faeae74f51a8
Mikita Taukachou <edloidas@gmail.com>
2017-01-05 18:55:10 +0300

    Update personal info

--- util/config/data.js
+++ util/config/data.js
@@ -13,11 +13,6 @@ module.exports = {
     cv: '#',
     accounts: [
       {
-        name: 'Blog',
-        url: '#',
-        title: 'Blog about JavaScript and 3d',
-      },
-      {
         name: 'GitHub',
         url: 'https://github.com/edloidas',
         title: '',
@@ -28,6 +23,16 @@ module.exports = {
         title: '',
       },
       {
+        name: 'Medium',
+        url: 'https://medium.com/@edloidas',
+        title: '',
+      },
+      {
+        name: 'Telegram',
+        url: 'https://telegram.me/edloidas',
+        title: '',
+      },
+      {
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/edloidas',
         title: '',
@@ -35,14 +40,24 @@ module.exports = {
     ],
     projects: [
       {
-        name: 'Dawn of Aegis',
-        url: 'https://github.com/edloidas/dawn-of-aegis',
-        title: 'JavaScript and WebGL game',
+        name: 'Penta Project',
+        url: 'https://github.com/edloidas/penta-project',
+        title: 'JavaScript and WebGL game (pre-alpha).',
       },
       {
-        name: 'Jetstream',
-        url: 'https://github.com/edloidas/jetstream',
-        title: 'Project template, based on Gulp and other tools',
+        name: 'Cheatsheets',
+        url: 'https://github.com/edloidas/cheatsheets',
+        title: 'Cheatsheets for different environments.',
+      },
+      {
+        name: 'runwith',
+        url: 'https://github.com/edloidas/runwith',
+        title: 'npm module to dynamically require other module and call it with a given arguments.',
+      },
+      {
+        name: 'rollrobot',
+        url: 'https://github.com/edloidas/rollrobot',
+        title: 'A Telegram bot that can roll the dice like no one before.',
       },
     ],
   },
